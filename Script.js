1f332de27859539f0e0ccc49093024fb4ea45e70
Lance Campos <lcampos20058@columbushs.com>
2020-05-09 16:59:09 -0400

    ?

---
+++ Script.js
@@ -0,0 +1,14 @@
+
+let redBox = document.getElementById("redBox");
+
+
+move (obj) {
+    if(obj.style.float = left){
+        obj.style.float = right;
+    };
+};
+
+
+redBox.onclick = () => {
+   redBox.style.backgroundColor = ("red");
+};
